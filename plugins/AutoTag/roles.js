831a0e81f4439d17ce226a00f21cb71d5bc215c9
Alex G <comicidiot@gmail.com>
2022-03-06 08:19:26 -0600

    New Locations Added
    Locations added to the auto-tagger

--- plugins/AutoTag/roles.js
+++ plugins/AutoTag/roles.js
@@ -24,4 +24,64 @@ export default [
         keywords: ["outsider"],
         roleId: "725740753459871815"
     },
+    {
+        name: "Byron",
+        keywords: ["byron"],
+        roleId: "838948139247337502"
+    },
+    {
+        name: "Kasson",
+        keywords: ["kasson"],
+        roleId: "948357008006996019"
+    },
+    {
+        name: "Mankato",
+        keywords: ["mankato"],
+        roleId: "948356788917514282"
+    },
+    {
+        name: "Austin",
+        keywords: ["austin"],
+        roleId: "948356703341117500"
+    },
+    {
+        name: "Pine Island",
+        keywords: ["pine island"],
+        roleId: "948356882874122341"
+    },
+    {
+        name: "Zumbrota",
+        keywords: ["Zumbrota"],
+        roleId: "950028461576163378"
+    },
+    {
+        name: "Twin Cities",
+        keywords: ["twin cities", "metro area", "Saint Paul", "St. Paul", "st Paul", "Minneapolis", "msp", "maple grove", "Bloomington", "Woodbury", "lakeville", "Shakopee", "eagan", "burnsville", "Blaine", "stillwater", "forest lake", "Eden Prairie", "Plymouth", "white bear lake", "Minnetonka", "inver", "apple valley"],
+        roleId: "948356824808181851"
+    },
+    {
+        name: "Stewartville",
+        keywords: ["Stewartville"],
+        roleId: "838947699809189920"
+    },
+    {
+        name: "Chatfield",
+        keywords: ["chatfield"],
+        roleId: "948356943100141588"
+    },
+    {
+        name: "St. Charles",
+        keywords: ["st charles", "st. Charles", "Saint Charles"],
+        roleId: "948357142698655764"
+    },
+    {
+        name: "Red Wing",
+        keywords: ["red wing"],
+        roleId: "950032215088169000"
+    },
+    {
+        name: "Wisconsin",
+        keywords: ["Wisconsin", "holmen", "Onalaska", "la crosse", "Fountain City", "Trempealeau", "Alma", "Hudson"],
+        roleId: "950032868153888819"
+    },
 ]
