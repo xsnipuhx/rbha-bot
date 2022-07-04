c2327724e47dee8fa19c0cb7b96436a706175e02
Alex G <comicidiot@gmail.com>
2022-07-04 11:01:25 -0500

    Removed spaces
    Now it'll recognize locations by themselves

--- plugins/AutoTag/roles.js
+++ plugins/AutoTag/roles.js
@@ -1,22 +1,22 @@
 export default [
     {
         name: "North West",
-        keywords: [" nw ", " north west ", " northwest "],
+        keywords: ["nw", "north west", "northwest"],
         roleId: "639820747594006528"
     },
     {
         name: "South West",
-        keywords: [" sw ", " south west ", " southwes t"],
+        keywords: ["sw", "south west", "southwest"],
         roleId: "639828501524119572"
     },
     {
         name: "South East",
-        keywords: [" se ", " south east ", " southeast "],
+        keywords: ["se", "south east", "southeast"],
         roleId: "668289414895632431"
     },
     {
         name: "North East",
-        keywords: [" ne ", " north east ", " northeast "],
+        keywords: ["ne", "north east", "northeast"],
         roleId: "668289317331927061"
     },
     {
