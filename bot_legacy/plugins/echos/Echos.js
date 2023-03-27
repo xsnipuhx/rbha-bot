87de8bed5ad929466b8519c162a5dd9a2d9c8440
xsnipuhx <xsnipuhx@gmail.com>
2023-03-27 15:11:55 -0500

    Update Echos.js

--- bot_legacy/plugins/echos/Echos.js
+++ bot_legacy/plugins/echos/Echos.js
@@ -7,38 +7,18 @@ export default function(bastion, config={}) {
     return [
 
         {
+            command: "pitchforks",
+            resolve: "ANGRY AT OP? WANT TO JOIN THE MOB? WE'VE GOT YOU COVERED! THE TRADITIONAL ---E | LEFT HANDED Ǝ--- | FANCY ---{ | CLEARANCE MODELS: ---F ---L ---e . HAPPY LYNCHING!"
+        },
+        
+        {
             command: "help",
             resolve: "Command has been moved to `!meetup` or `!meetup help`"
         },
 
         {
-            command: "scooter",
-            resolve: "<:coolscooter:443185468348170240><:coolscooter:443185468348170240>😎😎SCOOTER GANG SCOOTER GANG SCOOTER GANG😎😎<:coolscooter:443185468348170240><:coolscooter:443185468348170240>"
-        },
-
-        {
-            command: "scooters",
-            resolve: "<:coolscooter:443185468348170240><:coolscooter:443185468348170240>😎😎SCOOTER GANG SCOOTER GANG SCOOTER GANG😎😎<:coolscooter:443185468348170240><:coolscooter:443185468348170240>"
-        },
-
-        {
-            command: "swirls",
-            resolve: "<:swirls:654572567679598603><:swirls:654572567679598603><:swirls:654572567679598603><:swirls:654572567679598603>\:laughing:\:rofl:  DID SOMEBODY SAY SWIRLS?! \:rofl:\:laughing:<:swirls:654572567679598603><:swirls:654572567679598603><:swirls:654572567679598603><:swirls:654572567679598603>"
-        },
-
-        {
             command: "mention",
             resolve: "Commands been changed to `!meetup mention`"
-        },
-
-        {
-            command: "mocha",
-            resolve: "\:coffee:\:coffee: TIME FOR A MOCHA BREAK \:no_entry_sign:\:sleeping:"
-        },
-
-        {
-            command: "boba",
-            resolve: "\:cup_with_straw: \:regional_indicator_b: \:regional_indicator_o: \:regional_indicator_b: \:regional_indicator_a: \:alarm_clock: \:regional_indicator_t: \:regional_indicator_i: \:regional_indicator_m: \:regional_indicator_e: \:confetti_ball:"
         },
 
         {
@@ -82,17 +62,17 @@ export default function(bastion, config={}) {
 
         {
             command: "git",
-            resolve: "Github: https://github.com/hellos3b/sjbha-bot"
+            resolve: "Github: https://github.com/qCzar/rbha-bot"
         },
 
         {
             command: "github",
-            resolve: "Github: https://github.com/hellos3b/sjbha-bot"
+            resolve: "Github: https://github.com/qCzar/rbha-bot"
         },
 
         {
             command: "bug",
-            resolve: "Submit the bug here so I can keep track of them: https://github.com/hellos3b/sjbha-bot/issues/new"
+            resolve: "Submit the bug here so I can keep track of them: MESSAGE ALEX ^.^"
         }
 
         // {
