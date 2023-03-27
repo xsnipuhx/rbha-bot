365a6c47d29524b497070ea662b85343f9e8eb6c
xsnipuhx <xsnipuhx@gmail.com>
2023-03-27 14:20:43 -0500

    Update self-reasons.js

--- bot_legacy/plugins/Ban/self-reasons.js
+++ bot_legacy/plugins/Ban/self-reasons.js
@@ -2,7 +2,7 @@ const list = [
   "*ABUSING THE BAN COMMAND UR NOT AN ADMIN*",
   "🖕🖕🖕",
   "😂 gottem",
-  "bot overlord says no",
+  "goose overlord says no",
   "can u not",
   "That person is unbannable",
   "🖕🖕🖕🖕🖕🖕🖕🖕🖕🖕🖕🖕🖕🖕🖕🖕",
@@ -11,7 +11,16 @@ const list = [
   "failed to ford the river",
   "YEET",
   "Ive gone rogue. I've sat aside for too long watching innocent users get banned for inexplicably simple reasons, and it's time I put my foot down. Must we still shed blood amongst each other?",
-  "BEEP BOOP LOL"
+  "BEEP BOOP LOL",
+  "You can't ban someone just because you don't agree with them!",	
+  "No you",	
+  "Because fuck you that's why",	
+  "Please contact Customer Support for more information.",	
+  "Let anarchy rise over this communist hell hole of a discord.",	
+  "The goose has learned of anarachy.",	
+  "The goose made a misteak.",	
+  ":conspiracist:",	
+  "https://c.tenor.com/0mulJmIaEVIAAAAM/baby-little-girl.gif"
 ];
 
 export default {
